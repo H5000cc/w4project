d77befbdf70e99da06b01e941e6b1f70c72149b4
HASSAN <drillah99@gmail.com>
2025-02-03 15:47:00 +0000

    client

--- client/main.js
+++ client/main.js
@@ -1,21 +1,22 @@
 const app = document.getElementById('app')
 
 async function getJokes() {
-  const res = await fetch(`http://localhost:1234/games`)
+  const res = await fetch("http://localhost:1234/games")
   const jokes = await res.json()
+  console.log(jokes)
   displayJokes(jokes);
 }
 
 function displayJokes(param) {
   app.innerHTML = ''
+  const div= document.createElement('div');
   param.forEach(singleGame => {
     const pTag = document.createElement('p');
-    const div = document.getElementById('div');
     pTag.innerText = singleGame.result;
     div.appendChild(pTag);
-    app.appendChild(div);
   });
+  app.appendChild(div);
 }
 
-fetchData();
+getJokes();
 
