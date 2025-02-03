611dd303752de084ae1f32f6b5b17e75dcce4b47
HASSAN <drillah99@gmail.com>
2025-02-03 15:01:34 +0000

    2nd commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import pg, { Connection } from 'pg';
+import pg from 'pg';
 import dotenv from 'dotenv';
 // imports above 
 const app = express();
@@ -8,13 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) =>{
+    res.json('root')
+})
+
 dotenv.config();
 // connection to db[]
-const db = newpg.Pool({
+const db = new pg.Pool({
     connectionString: process.env.DB_CONN
 });
 
-app.get('/games', async (res, req) => {
+
+app.get('/games', async (req, res) => {
     const result = await db.query('SELECT *  FROM GAMES');
     const games = result.rows;
     res.json(games);
@@ -23,12 +28,14 @@ app.get('/games', async (res, req) => {
 app.post('/games', async (req, res) => {
 const gameFromReq = req.body.game;
 const resultFromReq = req.body.result;
-const data = await db.query("INSERT INTO games (game, result) VALUES ('${gameFromReq}', '${resultFromReq}')")
+const data = await db.query("INSERT INTO games (game, result) VALUES ($1, $2)",[
+    game, result
+])
 res.json(data)
 });
 
-app.listen('1234', () => {
-    console.log('App running on http://localhost:1234')
+app.listen(1234, () => {
+    console.log('App running on localhost:1234')
 });
 
 
